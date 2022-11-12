b5fd38b8a9307785d2180d5c4f0444de16345f2c
Chukwudubem Osegbe <lawrenceosegbe97@gmail.com>
2022-11-12 17:12:00 +0100

    adjust footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,9 +6,9 @@ import { FaLinkedinIn, FaTwitter, FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="bg-darkBG w-full py-20 px-5 md:px-10 lg:px-20 dark:bg-darkBG flex flex-col md:flex-row gap-10">
+    <footer className="bg-darkBG w-full py-20 px-5 md:px-10 lg:px-20 dark:bg-darkBG flex flex-col lg:flex-row gap-10">
       {/* Logo */}
-      <div className="md:w-[30%] w-full flex justify-start items-center">
+      <div className="md:w-[35%] w-full flex justify-start items-center">
         <Link href={"/"}>
           <Image
             src={SubshareLogo}
@@ -21,9 +21,9 @@ export default function Footer() {
       </div>
 
       {/* Foot Links */}
-      <div className="md:w-[40%] w-full flex flex-col md:flex-row gap-5 text-white md:justify-between md:items-center">
+      <div className="lg:w-[65%] w-full flex flex-col lg:flex-row gap-10 text-white md:justify-between md:items-center">
         {/* Legal */}
-        <div className="w-1/2 md:w-full">
+        <div className="lg:w-1/3 w-full">
           <h1 className="font-bold text-[1rem] text-main mb-8">Legal</h1>
           <div className="text-[0.85rem] font-medium my-4">
             <Link href={"/"}>Terms Of Service</Link>
@@ -34,24 +34,23 @@ export default function Footer() {
         </div>
 
         {/* Subshare */}
-        <div className="md:w-1/2 w-full">
+        <div className="lg:w-1/3 w-full">
           <h1 className="font-bold text-[1rem] text-main mb-8">Subshare</h1>
           <div className="text-[0.85rem] font-medium my-4">
             <Link href={"/#faqs"}>Faqs</Link>
           </div>
           <div className="text-[0.85rem] font-medium my-4">
-            <Link href={"/"}>Contact Us</Link>
+            <Link href={"/"}>About Us</Link>
           </div>
         </div>
-      </div>
 
-      {/* Others */}
-      <div className="md:w-[30%] w-full flex md:justify-end md:items-center">
-        <div className="text-white font-semibold">
-          <Link href={"/"}>support@usesubshare.com</Link>
-
-          {/* Social Icons */}
-          <div className="flex gap-7 my-7 text-main">
+        {/* Contact */}
+        <div className="lg:w-1/3 w-full">
+          <h1 className="font-bold text-[1rem] text-main mb-8">Contact</h1>
+          <div className="text-[0.85rem] font-medium my-4">
+            <Link href={"/#faqs"}>support@usesubshare.com</Link>
+          </div>
+          <div className="text-[0.85rem] font-medium my-4 flex gap-10 text-main">
             {/* LinkedIn */}
             <div className="hover:text-white">
               <Link href={"/"}>
